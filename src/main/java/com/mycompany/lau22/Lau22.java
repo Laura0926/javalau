21d5f6fea3309e8d8c438244966d0b2f639a026d
LauraNataliaQuintero <LauraNataliaQuintero@PC-1417-515GHF>
2026-09-22 14:22:29 -0500

    prueba

---
+++ src/main/java/com/mycompany/lau22/Lau22.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lau22;
+
+/**
+ *
+ * @author LauraNataliaQuintero
+ */
+public class Lau22 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
